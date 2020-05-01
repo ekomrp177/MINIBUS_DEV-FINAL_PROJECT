c915b97c234a304bbe312390d3cf035014862033
rilo.pambudi1998@gmail.com <45749606+ekomrp177@users.noreply.github.com>
2020-05-01 19:00:34 +0700

    Compare from Mobile Branch

---
+++ app/src/main/java/com/example/android/guidefortourism/viewmodel/search/SearchViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.android.guidefortourism.viewmodel.search
+
+import androidx.lifecycle.ViewModel
+
+class SearchViewModel: ViewModel() {
+    fun setSearch(){
+
+    }
+    fun getSearch(){
+        
+    }
+}
