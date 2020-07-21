144351363c491e96bc4cf5edf0d5eac6b04f48c5
rilo.pambudi1998@gmail.com <45749606+ekomrp177@users.noreply.github.com>
2020-07-21 13:26:50 +0700

    MVC TourAttraction

--- app/src/main/java/com/kcb/android/tourismguide/ui/tour/TourFragment.kt
+++ app/src/main/java/com/kcb/android/tourismguide/ui/tour/TourFragment.kt
@@ -5,9 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.kcb.android.tourismguide.R
+import com.kcb.android.tourismguide.adapter.RecyclerViewTourAdapter
+import com.kcb.android.tourismguide.parcelable.TourParcelable
+import com.kcb.android.tourismguide.viewmodel.tour.TourViewModel
+import kotlinx.android.synthetic.main.fragment_tour.*
 
 class TourFragment : Fragment() {
+    private lateinit var adapter: RecyclerViewTourAdapter
+    private lateinit var tourViewModel: TourViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,6 +29,21 @@ class TourFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        adapter = RecyclerViewTourAdapter()
+        adapter.notifyDataSetChanged()
+
+        recycleview.layoutManager = LinearLayoutManager(context)
+        recycleview.adapter = adapter
+
+        tourViewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory()).get(TourViewModel::class.java)
+
+        tourViewModel.setTourAttraction()
+        tourViewModel.getTourAttraction().observe(viewLifecycleOwner, Observer { TourParcelable ->
+            if(TourParcelable != null) {
+                adapter.setData(TourParcelable)
+                progressBar.visibility = View.INVISIBLE
+            }
+        })
         
     }
 }
